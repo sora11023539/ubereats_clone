0998c374f149dbd2ce9e2b1556e045a80b9090ba
sora11023539 <sora11023539@gmail.com>
2022-09-21 01:30:00 +0900

    add: Create line_foods_controller.rb

---
+++ backend/app/controllers/api/v1/line_foods_controller.rb
@@ -0,0 +1,49 @@
+module Api
+  module V1
+    class LineFoodsController < ApplicationController
+      def create
+        # If you already have a provisional order at another store
+        if LineFood.active.other_restaurant(ordered_food.restaurant.id).exists?
+          return render json: {
+            existing_restaurant: LineFood.other_restaurant(ordered_food.restaurant.id).first.restaurant.name,
+            new_restaurant: Food.find(params[:food_id]).restaurant.name,
+          },status: :not_acceptable
+        end
+
+        set_line_food(ordered_food)
+
+        if @line_food.save
+          render json: {
+            line_food: @line_food
+          },status: :created
+        else
+          render json: {}, status: :internal_server_error
+        end
+      end
+
+      private
+
+      def ordered_food
+        @ordered_food ||= Food.find(params[:food_id])
+      end
+
+      def set_line_food(ordered_food)
+        # If same foods exists
+        if ordered_food.line_food.present?
+          @line_food ||= ordered_food.line_food
+          # attributes = Can get the attributes
+          @line_food.attributes = {
+            count: ordered_food.line_food.count + params[:count],
+            active: true
+          }
+        else
+          @line_food = ordered_food.build_line_food(
+            count: params[:count],
+            restaurant: ordered_food.restaurant,
+            active: true
+          )
+        end
+      end
+    end
+  end
+end
