9c178014e7bf3941647123e9f358cde609b5cddb
sora11023539 <sora11023539@gmail.com>
2022-09-22 20:58:27 +0900

    fix: Refactoring foods_controller.rb

--- backend/app/controllers/api/v1/foods_controller.rb
+++ backend/app/controllers/api/v1/foods_controller.rb
@@ -2,12 +2,19 @@ module Api
   module V1
     class FoodsController < ApplicationController
       def index
-        restaurant = Restaurant.find(params[:restaurant_id])
-        foods = restaurant.foods
-
         render json: {
           foods: foods
         }, status: :ok
+      end
+
+      private
+
+      def restaurant
+        @restaurant ||= Restaurant.find(params[:restaurant_id])
+      end
+
+      def foods
+        @foods ||= restaurant.foods
       end
     end
   end
