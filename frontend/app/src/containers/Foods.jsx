a22836322bfcc7792f175a0248321c2b024ceeb2
sora11023539 <sora11023539@gmail.com>
2022-09-23 12:25:45 +0900

    add: Set routing

--- frontend/app/src/containers/Foods.jsx
+++ frontend/app/src/containers/Foods.jsx
@@ -1,7 +1,12 @@
 import React, { Fragment } from 'react';
 
-const Foods = () => {
-  return <Fragment>フード一覧</Fragment>;
+const Foods = ({ match }) => {
+  return (
+    <Fragment>
+      フード一覧
+      <p>restaurantIdは {match.params.restaurantId} です</p>
+    </Fragment>
+  );
 };
 
 export default Foods;
