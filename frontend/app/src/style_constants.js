770463fea5b37be250f0eed720d910b71b6b7bd2
sora11023539 <sora11023539@gmail.com>
2022-09-23 13:42:15 +0900

    add: Set constants and images

---
+++ frontend/app/src/style_constants.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  MAIN: '#01A457',
+  BORDER: '#E2E2E2',
+  SUB_TEXT: '#545454',
+  SUB_BUTTON: '#EEEEEE',
+}
+
+export const FONT_SIZE = {
+  STAND_BODY: '24px',
+  BODY1: '16px',
+  BODY2: '14px',
+}
