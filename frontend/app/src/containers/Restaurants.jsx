c0e0f8a56725fa3c14f4c1fa58dbecda64424d23
sora11023539 <sora11023539@gmail.com>
2022-09-24 22:35:11 +0900

    add: Styling restaurants index

--- frontend/app/src/containers/Restaurants.jsx
+++ frontend/app/src/containers/Restaurants.jsx
@@ -2,16 +2,26 @@
 import React, { Fragment, useEffect, useReducer } from 'react';
 import styled from 'styled-components';
 
+import { Link } from 'react-router-dom';
+
+// components
+import Skelton from '@material-ui/lab/Skeleton';
+
 // apis
 import { fetchRestaurants } from '../apis/restaurants';
+
+// constants
+import { REQUEST_STATE } from '../constants';
 
 // images
 import MainLogo from '../images/logo.png';
 import MainCoverImage from '../images/main-cover-image.png';
+import RestaurantImage from '../images/restaurant-image.jpg';
 
 // reducers
 import { initialState, restaurantsActionTypes, restaurantsReducer } from '../reducers/restaurants';
 
+// styled
 const HeaderWrapper = styled.div`
   display: flex;
   justify-content: flex-start;
@@ -30,6 +40,33 @@ const MainCover = styled.img`
   height: 600px;
 `;
 
+const RestaurantsContentList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 150px;
+`;
+
+const RestaurantsContentWrapper = styled.div`
+  width: 450px;
+  height: 300px;
+  padding: 48px;
+`;
+
+const RestaurantsImageNode = styled.img`
+  width: 100%;
+`;
+
+const MainText = styled.p`
+  color: black;
+  font-size: 18px;
+`;
+
+const SubText = styled.p`
+  color: black;
+  font-size: 12px;
+`;
+
+// function
 const Restaurants = () => {
   const [state, dispatch] = useReducer(restaurantsReducer, initialState);
   // useEffect = A function that is executed after the render result is reflected on the screen
@@ -56,9 +93,25 @@ const Restaurants = () => {
         <MainCover src={MainCoverImage} alt="main cover" />
       </MainCoverImageWrapper>
 
-      {state.restaurantsList.map((restaurant) => (
-        <div key={restaurant.id}>{restaurant.name}</div>
-      ))}
+      <RestaurantsContentList>
+        {state.fetchState === REQUEST_STATE.LOADING ? (
+          <Fragment>
+            <Skelton variant="rect" width={450} height={300} />
+            <Skelton variant="rect" width={450} height={300} />
+            <Skelton variant="rect" width={450} height={300} />
+          </Fragment>
+        ) : (
+          state.restaurantsList.map((item, index) => (
+            <Link to={`/restaurants/${item.id}/foods`} key={index} style={{ textDecoration: 'none' }}>
+              <RestaurantsContentWrapper>
+                <RestaurantsImageNode src={RestaurantImage} />
+                <MainText>{item.name}</MainText>
+                <SubText>{`配送料:${item.fee}円 ${item.time_required}分`}</SubText>
+              </RestaurantsContentWrapper>
+            </Link>
+          ))
+        )}
+      </RestaurantsContentList>
     </Fragment>
   );
 };
