ca50d328d2566a3ef10d5008f10555ecde5df4e0
sora11023539 <sora11023539@gmail.com>
2022-09-25 01:23:07 +0900

    add: Increase loading time

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :fake_load, if: :env_development
+
+  def env_development
+    Rails.env.development?
+  end
+
+  # For checking SPA behavior
+  def fake_load
+    sleep(1)
+  end
 end
