458f1aa6a59ed6d1185e91786f103c68671268db
sora11023539 <sora11023539@gmail.com>
2022-09-24 19:15:16 +0900

    add: Styling header

--- frontend/app/src/containers/Restaurants.jsx
+++ frontend/app/src/containers/Restaurants.jsx
@@ -1,18 +1,51 @@
 // Fragment = Remove the constraint that only one element can be returned
 import React, { Fragment, useEffect } from 'react';
+import styled from 'styled-components';
+
+// apis
 import { fetchRestaurants } from '../apis/restaurants';
 
+// images
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
+
 const Restaurants = () => {
+  // useEffect = A function that is executed after the render result is reflected on the screen
   useEffect(() => {
     fetchRestaurants()
     .then((data) =>
       console.log(data)
-    )
-  })
+    );
+  },[])
 
-  return <Fragment>レストラン一覧</Fragment>;
+  return (
+    <Fragment>
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+    </Fragment>
+  )
 };
 
 export default Restaurants;
-
-// useEffect = A function that is executed after the render result is reflected on the screen
