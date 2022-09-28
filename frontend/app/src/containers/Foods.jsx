cf5e363d12a765f9df4cefc2c54e1c80656521a4
sora11023539 <sora11023539@gmail.com>
2022-09-29 00:54:09 +0900

    add: useReducer

--- frontend/app/src/containers/Foods.jsx
+++ frontend/app/src/containers/Foods.jsx
@@ -1,14 +1,49 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
+
+// reducers
+// A as B = Can be reference A as B
+import { initialState as foodsInitialState, foodsActionTypes, foodsReducer } from '../reducers/foods';
+
+// apis
 import { fetchFoods } from '../apis/foods';
+
+// constants
+import { REQUEST_STATE } from '../constants';
+
 import { useParams } from 'react-router-dom';
 
 const Foods = () => {
   let { restaurantsId } = useParams();
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
   useEffect(() => {
-    fetchFoods(restaurantsId).then((data) => console.log(data));
+    dispatch({ type: foodsActionTypes.FETCHING });
+    fetchFoods(restaurantsId).then((data) => {
+      dispatch({
+        type: foodsActionTypes.FETCH_SUCCESS,
+        payload: {
+          foods: data.foods,
+        },
+      });
+    });
   }, [restaurantsId]);
 
-  return <Fragment>フード一覧</Fragment>;
+  return (
+    <Fragment>
+      {
+        foodsState.fetchState === REQUEST_STATE.LOADING?
+          <Fragment>
+            <p>ロード中...</p>
+          </Fragment>
+        :
+          foodsState.foodsList.map(food =>
+            <div key={food.id}>
+              {food.name}
+            </div>
+          )
+      }
+      </Fragment>
+  )
 };
 
 export default Foods;
