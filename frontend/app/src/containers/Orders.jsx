8978270a64303f77431239ad5b274b8b43343174
sora11023539 <sora11023539@gmail.com>
2022-10-02 05:51:41 +0900

    add: Get api for line foods

--- frontend/app/src/containers/Orders.jsx
+++ frontend/app/src/containers/Orders.jsx
@@ -1,14 +1,25 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
 import { fetchLineFoods } from '../apis/line_foods';
 
+// reducers
+import { initialState, lineFoodsActionTypes, lineFoodsReducer } from '../reducers/lineFoods';
+
 const Orders = () => {
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+
   useEffect(() => {
+    dispatch({ type: lineFoodsActionTypes.FETCHING });
     fetchLineFoods()
       .then((data) =>
-        console.log(data)
+        dispatch({
+          type: lineFoodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data,
+          },
+        })
       )
       .catch((e) => console.error(e));
-  },[])
+  }, []);
   return <Fragment>注文画面</Fragment>;
 };
 
